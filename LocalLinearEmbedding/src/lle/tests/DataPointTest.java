977841a2992488d18c1e40239bb33871b6d01b3d
Christopher Drews <christopher.drews@lhind.dlh.de>
2016-01-08 16:54:49 +0100

    implement DataPoint tests

--- LocalLinearEmbedding/src/lle/tests/DataPointTest.java
+++ LocalLinearEmbedding/src/lle/tests/DataPointTest.java
@@ -4,21 +4,47 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import lle.DataPoint;
+
 public class DataPointTest {
 
 	@Test
 	public void testGetAllDimensions() {
-		fail("Not yet implemented");
+		//given
+		double[] a = {1.,2.,3.};
+		DataPoint b= new DataPoint(a);
+		//when
+		double[] c = b.getAllDimensions();
+		//then
+		assertEquals(1.,c[0], 0.00000001);
+		assertEquals(2.,c[1], 0.00000001);
+		assertEquals(3.,c[2], 0.00000001);
 	}
 
 	@Test
 	public void testGetDimensionN() {
-		fail("Not yet implemented");
+		//given
+		double[] a = {1.,2.,3.};
+		DataPoint b= new DataPoint(a);
+		//when
+		double c = b.getDimensionN(0);
+		double d = b.getDimensionN(1);
+		double e = b.getDimensionN(2);
+		//then
+		assertEquals(1.,c, 0.00000001);
+		assertEquals(2.,d, 0.00000001);
+		assertEquals(3.,e, 0.00000001);
 	}
 
 	@Test
 	public void testGetNumberOfDimensions() {
-		fail("Not yet implemented");
+		//given
+		double[] a = {1.,2.,3.};
+		DataPoint b= new DataPoint(a);
+		//when
+		double c = b.getNumberOfDimensions();
+		//then
+		assertEquals(3.,c, 0.00000001);
 	}
 
 }
