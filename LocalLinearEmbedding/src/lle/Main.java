5afce27c31230b9d213dc0cd21ff2935b2a0678d
LilaLaura <burgmann.laura@gmail.com>
2016-01-17 18:01:19 +0100

    change output format

--- LocalLinearEmbedding/src/lle/Main.java
+++ LocalLinearEmbedding/src/lle/Main.java
@@ -20,15 +20,21 @@ public class Main {
 		ArrayList<DataPoint> myDataPoints = parser.parseFile2DataPoints();
 
 		//Initialize LLE class which will do the computing
-		LLE myLLE = new LLE(myDataPoints, 25,3);
+		LLE myLLE = new LLE(myDataPoints, 15,3);
 		Matrix embeddedMatrix= new Matrix(myLLE.embeddingMatrix);
 		
 		File outputfile = new File ("testData/output.csv");
-	    PrintWriter printWriter;
 		try {
-			printWriter = new PrintWriter (outputfile);
-			embeddedMatrix.print(printWriter, 3, 5);
-			 printWriter.close ();    
+			PrintWriter printWriter=new PrintWriter(outputfile);
+			for(int i=0; i<=myLLE.embeddingMatrix.length-1; i++){
+				String temp="";
+				for(int j=0; j<=myLLE.embeddingMatrix[0].length-1; j++){
+					temp+= myLLE.embeddingMatrix[i][j];
+					temp+=";";
+				}
+				printWriter.write(temp+"\r\n");
+			}
+			printWriter.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block   
 			e.printStackTrace();
